1fbb9f6ff1d1dbaedb9658649e4b42421a6b395e
Minhaj Mahim <minhaj.thecoder@gmail.com>
2024-07-07 22:20:24 +0600

    updated

--- database.php
+++ database.php
@@ -3,8 +3,10 @@ $hostName = "localhost";
 $dbUser = "root";
 $dbPassword = "";
 $dbName = "login_register";
+
 $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
 if(!$conn){
     die("Something went wrong!");
 }
+
 ?>
