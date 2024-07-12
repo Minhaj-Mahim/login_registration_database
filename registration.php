99457c7280ebd58b5b9df2a8fb202982b8e48350
Minhaj Mahim <minhaj.thecoder@gmail.com>
2024-07-12 21:59:40 +0600

    regex_added

--- registration.php
+++ registration.php
@@ -1,10 +1,105 @@
-<?php
-session_start();
-if(isset($_SESSION["user"])){
-    header("Location: index.php");
-}
-?>
 <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <?php
+        if(isset($_POST["submit"])){
+            $fullname = $_POST["fullname"];
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            $confirm_password = $_POST["confirm_password"];
+
+            $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+            $errors = array();
+
+            if(empty($fullname) OR empty($email) OR empty($password) OR empty($confirm_password)){
+                array_push($errors, "All fields are required");
+            }
+            if (!preg_match("/^[a-zA-Z\s]+$/", $fullname)) {
+                array_push($errors, "Full name should only contain characters");
+            }
+            if (!preg_match("/^[a-zA-Z0-9._%+-]+@(gmail\.com|yahoo\.com|outlook\.com|cse_|eee_|law_|eng_|arc_\d{10,16}@lus\.ac\.bd)$/", $email)) {
+                array_push($errors, "Email is not valid");
+            }
+            if (strlen($password)<8 || !preg_match("/[A-Z]/", $password) || !preg_match("/\d/", $password) || !preg_match("/[\W]/", $password)) {
+                array_push($errors, "Password must be at least 8 characters long, contain at least one capital letter, one digit, and one special character");
+            }
+            if ($password !== $confirm_password) {
+                array_push($errors,"Password does not match!");
+            }
+
+            require_once "database.php";
+            $sql = "SELECT * FROM users WHERE email = '$email'";
+            $result = mysqli_query($conn, $sql);
+            $rowCount = mysqli_num_rows($result);
+            if ($rowCount>0) {
+                array_push($errors, "Email already exists!");
+            }
+            if (count($errors)>0) {
+                foreach ($errors as $error) {
+                    echo "<div class='alert alert-danger'>$error</div>";
+                }
+            } else{
+                
+                $sql = "INSERT INTO users(full_name, email, password) VALUES ( ? , ? , ? )";
+                $stmt = mysqli_stmt_init($conn);
+                $prepareStmt = mysqli_stmt_prepare($stmt, $sql);
+                if($prepareStmt){
+                    mysqli_stmt_bind_param($stmt,"sss",$fullname,$email,$passwordHash);
+                    mysqli_stmt_execute($stmt);
+                    echo"<div class='alert alert-success'>You are registered successfully.</div>";
+                }
+                else{
+                    die("Something went wrong");
+                }
+            }
+        }
+        ?>
+        <form action="registration.php" method="post">
+            <div class="form-group">
+                <input type="text" class="form-control" name="fullname" placeholder="Full Name:">
+            </div>
+            <div class="form-group">
+                <input type="email" class="form-control" name="email" placeholder="Email:">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" name="password" placeholder="Password:">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password:">
+            </div>
+            <div class="form-btn">
+                <input type="submit" class="btn btn-primary" value="Register" name="submit">
+            </div>
+        </form>
+        <div><p>Already Registered! <a href="login.php">Login Here</a></p></div>
+    </div>
+    <!--<script src="validation.js"></script>-->
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- 
+ <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -59,7 +154,6 @@ if(isset($_SESSION["user"])){
                     mysqli_stmt_bind_param($stmt,"sss",$fullname,$email,$passwordHash);
                     mysqli_stmt_execute($stmt);
                     echo"<div class='alert alert-success'>You are registered successfully.</div>";
-                    header("Location: login.php");
                 }
                 else{
                     die("Something went wrong");
@@ -86,5 +180,7 @@ if(isset($_SESSION["user"])){
         </form>
         <div><p>Already Registered! <a href="login.php">Login Here</a></p></div>
     </div>
+    <script src="validation.js"></script>
 </body>
 </html>
+-->
